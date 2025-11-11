8d901aedd09da903da8c1f28f0164b503fbf6ba9
bayramBabagulyyew <babakulyyewbayram8@gmail.com>
2025-11-11 21:03:12 +0500

    :bug: fix: some bugs fixed

--- src/modules/about/about.mapper.ts
+++ src/modules/about/about.mapper.ts
@@ -6,21 +6,20 @@ export class AboutMapper {
   public static toClient(model: AboutModel, lang: Lang) {
     const dto = new ResponseAboutDto();
     dto.id = model.id ?? null;
-    dto.title = model[`title_${lang}`]
-      ? model[`title_${lang}`]
+    dto.title = model.dataValues[`title_${lang}`]
+      ? model.dataValues[`title_${lang}`]
       : model.title_tk;
-    dto.text = model[`text_${lang}`] ? model[`text_${lang}`] : model.text_tk;
-
+    dto.text = model.dataValues[`text_${lang}`] ? model.dataValues[`text_${lang}`] : model.text_tk;
     dto.createdAt = model.createdAt ?? null;
     dto.updatedAt = model.updatedAt ?? null;
-    dto.title_tk = model.title_tk;
-    dto.title_en = model.title_en;
-    dto.title_ru = model.title_ru;
-    dto.text_tk = model.text_tk;
-    dto.text_en = model.text_en;
-    dto.text_ru = model.text_ru;
-    dto.artistName = model.artistName;
-    dto.job = model.job;
+    dto.title_tk = model.dataValues.title_tk;
+    dto.title_en = model.dataValues.title_en;
+    dto.title_ru = model.dataValues.title_ru;
+    dto.text_tk = model.dataValues.text_tk;
+    dto.text_en = model.dataValues.text_en;
+    dto.text_ru = model.dataValues.text_ru;
+    dto.artistName = model.dataValues.artistName;
+    dto.job = model.dataValues.job;
     return dto;
   }
 }
